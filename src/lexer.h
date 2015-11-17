65bcf2647cd8924a74da7c4aec622b7f7c11b170
John Powell <jepyes@gmail.com>
2015-11-17 13:23:23 -0600

    removed the EBNF comments from lexer.h

--- src/lexer.h
+++ src/lexer.h
@@ -9,28 +9,6 @@
 
 #include "scanner.h"
 
-/*
- * EBNF
- *
- * whitespace = " " | "\t" | "\n" ;
- *
- * digit = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" ;
- *
- * letter = "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j"
- *        | "k" | "l" | "m" | "n" | "o" | "p" | "q" | "r" | "s" | "t"
- *        | "u" | "v" | "w" | "x" | "y" | "z" | "A" | "B" | "C" | "D"
- *        | "E" | "F" | "G" | "H" | "I" | "J" | "K" | "L" | "M" | "N"
- *        | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" | "W" | "X"
- *        | "Y" | "Z" ;
- *
- * identifier start = letter | "$" | "_"
- *
- * symbol = "#" | "\"" | ";" | "+" | "-"  | "*" | "/" | "=" | "!" | "<"
- *          ">" | "&" | "|" | "==" | "+=" | "-=" | "*=" | "/=" | "++"
- *          "--" | "<=" | ">=" | "&&" | "||" ;
- *
- */
-
 extern const char WHITESPACE[3];
 extern const char DIGIT[10] ;
 extern const char LETTER[52];
