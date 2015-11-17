eca0fd75ad0f68b184d08c0b0a0a55ceca7e90e6
John Powell <jepyes@gmail.com>
2015-11-17 09:17:58 -0600

    moved expression evaluation functions to parser files

--- src/parser.c
+++ src/parser.c
@@ -130,3 +130,233 @@ void Block(tstream_t* stream)
 		Statement(stream);
 	}
 }
+
+void Push(stack_t* stack, char* value)
+{
+	int value_size = 0;
+	while(value[value_size] != '\0')
+		value_size++;
+
+	if(stack->size < stack->capacity)
+	{
+		stack->data[stack->size] = malloc(sizeof(char) * (value_size + 1));
+		int i;
+		for(i = 0; i < value_size; i++)
+			stack->data[stack->size][i] = value[i];
+		stack->data[stack->size++][i] = '\0';
+	}
+	else
+	{
+		char** temp_data = malloc(sizeof(char*) * stack->capacity);
+		int i;
+		for(i = 0; i < stack->capacity; i++)
+			temp_data[i] = stack->data[i];
+
+		free(stack->data);
+		stack->data = malloc(sizeof(char*) * (stack->capacity + stack->capacity / 2));
+		
+		for(i = 0; i < stack->capacity; i++)
+			stack->data[i] = temp_data[i];
+
+		free(temp_data);
+		stack->data[stack->size] = malloc(sizeof(char) * (value_size + 1));
+
+		for(i = 0; i < value_size; i++)
+			stack->data[stack->size][i] = value[i];
+		stack->data[stack->size++][i] = '\0';
+		stack->capacity = stack->capacity + stack->capacity / 2;
+	}
+}
+
+void Pop(stack_t* stack, char** value)
+{
+	if(stack->size)
+		*value = stack->data[stack->size-- - 1];
+}
+
+void PopAll(stack_t* stack)
+{
+	int i;
+	for(i = 0; i < stack->size; i++)
+		free(stack->data[i]);
+	free(stack->data);
+	stack->size = 0;
+	stack->capacity = 10;
+	stack->data = malloc(sizeof(char*) * stack->capacity);
+}
+
+void PrintStack(stack_t* stack)
+{
+	int i;
+	for(i = 0; i < stack->size; i++)
+		printf("%s, ", stack->data[i]);
+	printf("\n");
+}
+
+int Priority(char* value)
+{
+	if(!strcmp(value, "+") || !strcmp(value, "-"))
+		return 1;
+	else if(!strcmp(value, "*") || !strcmp(value, "/"))
+		return 2;
+	else if(!strcmp(value, "(") || !strcmp(value, ")"))
+		return 4;
+	else return 0;
+}
+
+void EvaluateBinaryOperation(char** opr,  char** l_operand, char** r_operand, char** result)
+{
+	char l_buffer[30];
+	char r_buffer[30];
+	char result_buffer[30];
+	int l_op;
+	int r_op; 
+	sscanf(*l_operand, "%d", &l_op);
+	sscanf(*r_operand, "%d", &r_op);
+	int i = 0;
+
+	if(!strcmp(*opr, "+"))
+	{
+		sprintf(result_buffer, "%d", l_op + r_op);
+		*result = malloc(sizeof(char) * 30);
+		while(result_buffer[i] != '\0')
+			(*result)[i] = result_buffer[i++];
+		(*result)[i] = result_buffer[i];
+	}
+	else if(!strcmp(*opr, "-"))
+	{
+		sprintf(result_buffer, "%d", l_op - r_op);
+		*result = malloc(sizeof(char) * 30);
+		while(result_buffer[i] != '\0')
+			(*result)[i] = result_buffer[i++];
+		(*result)[i] = result_buffer[i];
+	}
+	else if(!strcmp(*opr, "*"))
+	{
+		sprintf(result_buffer, "%d", l_op * r_op);
+		*result = malloc(sizeof(char) * 30);
+		while(result_buffer[i] != '\0')
+			(*result)[i] = result_buffer[i++];
+		(*result)[i] = result_buffer[i];
+	}
+	else if(!strcmp(*opr, "/"))
+	{
+		sprintf(result_buffer, "%d", l_op / r_op);
+		*result = malloc(sizeof(char) * 30);
+		while(result_buffer[i] != '\0')
+			(*result)[i] = result_buffer[i++];
+		(*result)[i] = result_buffer[i];
+	}
+
+}
+
+void Interpret(token_t* token, stack_t* expressions, stack_t* operators)
+{
+	while(strcmp(token->value, "end of stream"))
+	{
+		if(!strcmp(token->type, "number") 
+			|| !strcmp(token->type, "identifier"))
+		{
+			Push(expressions, token->value);
+		}
+		else if(!strcmp(token->type, "symbol")
+			&& strcmp(token->value, "{") 
+			&& strcmp(token->value, "}")
+			&& strcmp(token->value, ";"))
+		{
+			if(operators->size)
+			{
+				if(Priority(token->value) <= Priority(operators->data[operators->size - 1]))
+				{
+					while(operators->size > 0 && strcmp(operators->data[operators->size - 1], "("))
+					{
+						char* l_op;
+						char* r_op;
+						char* opr;
+						char* result; // malloc
+						Pop(expressions, &r_op);
+						Pop(expressions, &l_op);
+						Pop(operators, &opr);
+
+						EvaluateBinaryOperation(&opr, &l_op, &r_op, &result);
+									
+						free(expressions->data[expressions->size + 1]);
+						free(expressions->data[expressions->size]);
+						free(operators->data[operators->size]);
+
+						Push(expressions, result);
+						free(result);
+					}
+								
+					Push(operators, token->value);
+				}
+				else if(!strcmp(token->value, ")"))
+				{
+					while(operators->size > 0 && strcmp(operators->data[operators->size - 1], "("))
+					{
+						char* l_op;
+						char* r_op;
+						char* opr;
+						char* result; // malloc
+						Pop(expressions, &r_op);
+						Pop(expressions, &l_op);
+						Pop(operators, &opr);
+
+						EvaluateBinaryOperation(&opr, &l_op, &r_op, &result);
+									
+						free(expressions->data[expressions->size + 1]);
+						free(expressions->data[expressions->size]);
+						free(operators->data[operators->size]);
+
+						Push(expressions, result);
+						free(result);
+					}
+					if(!operators->size)
+					{
+						printf("mismatched parentheses\n");
+						break;
+					}
+					if(!strcmp(operators->data[operators->size - 1], "("))
+					{
+						char* l_paren;
+						Pop(operators, &l_paren);
+						free(operators->data[operators->size]);
+					}
+					else
+						printf("mismatched parentheses\n");
+				}
+				else
+					Push(operators, token->value);
+			}
+			else
+				Push(operators, token->value);
+		}
+		else if(!strcmp(token->value, ";"))
+		{
+			while(operators->size > 0)
+			{
+				char* l_op;
+				char* r_op;
+				char* opr;
+				char* result; // malloc
+				Pop(expressions, &r_op);
+				Pop(expressions, &l_op);
+				Pop(operators, &opr);
+
+				EvaluateBinaryOperation(&opr, &l_op, &r_op, &result);
+
+				free(expressions->data[expressions->size + 1]);
+				free(expressions->data[expressions->size]);
+				free(operators->data[operators->size]);
+
+				Push(expressions, result);
+				free(result);
+			}
+			PopAll(operators);
+			printf("expressions: ");
+			PrintStack(expressions);
+			PopAll(expressions);
+		}
+		token++;
+	}
+}
--- src/parser.h
+++ src/parser.h
@@ -3,26 +3,19 @@
 
 #include "lexer.h"
 
-typedef enum NodeType 
-{
-	ROOT, INTEGER, OPERATOR, CONSTANT, MISC
-} ntype_t;
-
-typedef struct AbstractSyntaxNode
-{
-	char* value;
-	ntype_t type;
-	int child_count;
-	struct AbstractSyntaxNode* child_nodes;
-} ast_node_t;
-
 typedef struct TokenStream
 {
 	token_t* next;
 	int indent;
 	int syntax_error;
-	ast_node_t* ast_node;
 } tstream_t;
+
+typedef struct Stack
+{
+	char** data;
+	int capacity;
+	int size;
+} stack_t;
 
 void NextToken(tstream_t* stream);
 int Accept(const char* expected, tstream_t* stream);
@@ -32,5 +25,13 @@ void Term(tstream_t* stream);
 void Expression(tstream_t* stream);
 void Statement(tstream_t* stream);
 void Block(tstream_t* stream);
+
+void Push(stack_t* stack, char* value);
+void Pop(stack_t* stack, char** value);
+void PopAll(stack_t* stack);
+int Priority(char* token);
+void PrintStack(stack_t* stack);
+void EvaluateBinaryOperation(char** opr, char** l_operand, char** r_operand, char** result);
+void Interpret(token_t* token, stack_t* expressions, stack_t* operators);
 
 #endif
